3bf8547eb69622fef919637d6b0e32fdf989af78
WildyWyrm <brian_graviti@hotmail.com>
2022-10-13 04:17:21 -0300

    first commit

---
+++ js/chrono.js
@@ -0,0 +1,40 @@
+let txt = document.getElementById("txt");
+segundos = 0;
+minutos = 0;
+horas = 0;
+flag = false;
+function refreshInterval() {
+    intervalID = setInterval(Cronometro, 1000);
+}
+
+function Cronometro() {
+    segundos++;
+    if (segundos >= 60) {
+        segundos = 0;
+        minutos++;
+        if (min >= 60) {
+            min = 0;
+            horas++;
+        }
+    }
+    txt.innerText = horas + ":" + minutos + ":" + segundos;
+}
+function start() {
+    if (flag == false) {
+        refreshInterval();
+    }
+    flag = true;
+}
+
+function reset() {
+    segundos = 0;
+    minutos = 0;
+    horas = 0;
+    txt.innerText = horas + ":" + minutos + ":" + segundos;
+}
+function stop() {
+    clearInterval(intervalID);
+    flag = false;
+}
+
+txt.innerText = horas + ":" + minutos + ":" + segundos;
